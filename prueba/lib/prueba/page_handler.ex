4bcfda6ca411d054ed83a39df3ac8d7ba84b4367
luismarquez.ortiz94@gmail.com <daniel.marquez@santiago.mx>
2022-02-24 00:37:01 -0600

    calculate salario_completo and print result

--- prueba/lib/prueba/page_handler.ex
+++ prueba/lib/prueba/page_handler.ex
@@ -1,9 +1,82 @@
 defmodule Prueba.PageHandler do
+  @moduledoc """
+    The module is used to obtain the salary of the players of "Resuelve FC"
+  """
 
-  def get_base_salary(map) do
-    map
-      |> Map.get(:sueldo)
+  @doc """
+  Calculates the sum of the values
+  Returns goals value :: int
+  ## Parameters
+    - team_goal_data: List of maps, contains value as key
+  ## Example
+    iex> total_team_goals( [%{..., value: 10, ...}, %{..., value: 8, ...}...])
 
+  """
+  def total_team_goals(team_goal_data) do
+    total_team_goals =
+      team_goal_data
+      |> Enum.map(fn f -> f.value end)
+      |> Enum.sum
+  end
+
+  @doc """
+  Calculates the sum of the "goles"
+
+  Returns goals value :: int
+    ##Parameters
+     - individual_goal_data: List of maps, contains "goles" as key
+    ## Example
+        iex> total_individual_goals([%{..., "goles":19}, %{...,"goles":19}, ...])
+
+  """
+  def total_individual_goals(individual_goal_data) do
+    total_individual_goals =
+      individual_goal_data
+      |> Enum.map(fn f -> f["goles"] end)
+      |> Enum.sum
+  end
+  @doc """
+  Return value where map contains level parameter
+    Returns goals value by level :: int
+    ## Parameters
+    - level: the level that serves as a key to search on the map
+    - data: map where to search
+    ## Example
+      get_level_value(level,  [%{..., level: "A", value: 10, ...}, %{..., level: "B", value: 8, ...}...])
+
+  """
+  def get_level_value(level, data) do
+    data
+    |> Enum.find(fn m -> m.level == level end)
+    |> case do
+      nil ->
+        IO.puts("No level found, adding 0")
+        0
+      any -> any.value
+    end
+  end
+
+  @doc """
+    Returns a list of maps adding the value "sueldo completo"
+    :: List
+    ## Parameters
+    - json_parced: list of maps to modify, coming from a json decode
+    - resuelveFC_map: map list containing "nivel" and "goles" information as a key
+    ## Example
+      get_level_value([%{..., "salario_completo": nil, goles: 10, ...}, %{...,"salario_completo": nil, level: "B", goles: 8, ...}...],
+      [%{..., level: "A", value: 10, ...}, %{..., level: "B", value: 8, ...}...])
+
+  """
+  def calculate_total_salary(json_parced, resuelveFC_map) do
+    total_team_goal = total_team_goals(resuelveFC_map)
+    total_individual_goal = total_individual_goals(json_parced["jugadores"])
+    json_out = json_parced["jugadores"]
+    |> Enum.map(fn f -> f |> Map.put("sueldo_completo",
+     (
+      (((f["goles"] / (f["nivel"] |> get_level_value(resuelveFC_map)) +
+       (total_individual_goal/total_team_goal)) / 2 ) * f["bono"]  )
+        ) + f["sueldo"] )
+    end)
   end
 
 end
--- prueba/lib/prueba_web/live/page_live.ex
+++ prueba/lib/prueba_web/live/page_live.ex
@@ -1,47 +1,50 @@
 defmodule PruebaWeb.PageLive do
   use PruebaWeb, :live_view
+  alias Prueba.PageHandler
 
   def mount(_params, _session, socket) do
     {:ok, assign(socket,
-      json_out: "",
       json_in: "",
+      json_print: "",
       resuelveFC_goal: individual_goal_data()
     )}
   end
 
   def handle_event("update_json", %{"text_area_json" => json}, socket) do
-
-
     {:noreply, assign(socket, json_in: json)}
-
   end
 
   def handle_event("send_json", _params, socket) do
-    json_out = socket.assigns.json_out
     json = socket.assigns.json_in
+    resuelveFC_goal = socket.assigns.resuelveFC_goal
     json
-    |> Poison.decode()
+      |> Poison.decode()
+        |> case do
+          {:ok, data} ->
+              calculate_total_salary(data, resuelveFC_goal)
+              |> Enum.map(fn f -> f |> Map.put("sueldo_completo", f["sueldo_completo"] |>  Float.to_string(decimals: 2) ) end)
+              |> Poison.encode!
+              |> case do
+                data ->
+                  {:noreply, assign(socket, json_print: data)}
+                _any ->
+                  {:noreply, assign(socket, json_print: "error processing")}
+              end
+          {:error, _any} ->
+              {:noreply, assign(socket, json_print: "Error: json malformed")}
+        end
+  end
 
-    |> case do
-      {:ok, data} -> json_out = data
-          json_out = json_out |> Poison.encode
-          {k, v} = json_out
-        {:noreply, assign(socket, json_out: v )}
-      {:error, _any} ->
-        {:noreply, assign(socket, json_out: "Error: json malformed"  )}
-
-    end
-
-
+  defp calculate_total_salary(json_parced, resuelveFC_map ) do
+    json_parced |> PageHandler.calculate_total_salary(resuelveFC_map)
   end
 
   defp individual_goal_data() do
     [
-    %{name: "A", value: 5},
-    %{name: "B", value: 10},
-    %{name: "C", value: 15},
-    %{name: "Cuauh", value: 20}
+      %{level: "A", value: 5},
+      %{level: "B", value: 10},
+      %{level: "C", value: 15},
+      %{level: "Cuauh", value: 20}
     ]
   end
-
 end
